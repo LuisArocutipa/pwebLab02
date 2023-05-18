5dcf5bf484b38ceb0d9b2cee52e602e1154829b2
LuisArocutipa <69124601+LuisArocutipa@users.noreply.github.com>
2023-05-17 22:27:56 -0500

    Se borró una linea en la primera función

--- Ejercicios.js
+++ Ejercicios.js
@@ -1,7 +1,6 @@
 function verDia(){
     var dias = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
     const dia = new Date().getDay();
-    console.log(dia)
     document.getElementById("ejercicio1").innerHTML = dias[dia];
 }
 function invertir(){
